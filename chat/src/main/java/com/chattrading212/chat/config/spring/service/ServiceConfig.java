6bf03555f6142e9714bce728e156391f172ed031
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-03 13:44:20 +0200

    change service config

--- chat/src/main/java/com/chattrading212/chat/config/spring/service/ServiceConfig.java
+++ chat/src/main/java/com/chattrading212/chat/config/spring/service/ServiceConfig.java
@@ -1,4 +1,14 @@
 package com.chattrading212.chat.config.spring.service;
 
+import com.chattrading212.chat.repositories.UserRepository;
+import com.chattrading212.chat.services.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ServiceConfig {
+    @Bean
+    public UserService userService(UserRepository repository) {
+        return new UserService(repository);
+    }
 }
--- chat/src/main/java/com/chattrading212/chat/services/UserService.java
+++ chat/src/main/java/com/chattrading212/chat/services/UserService.java
@@ -1,12 +1,14 @@
 package com.chattrading212.chat.services;
 
+import com.chattrading212.chat.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class UserService implements UserDetailsService{
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+public class UserService{
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
     }
 }
