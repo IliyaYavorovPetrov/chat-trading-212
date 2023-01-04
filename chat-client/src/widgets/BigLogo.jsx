f6edda4d5c151da91d97a92df42d80d7273f46cd
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-04 15:43:27 +0200

    update ThemeIcon coloer

--- chat-client/src/widgets/BigLogo.jsx
+++ chat-client/src/widgets/BigLogo.jsx
@@ -4,7 +4,7 @@ function BigLogo() {
   return (
     <h3 className="text-5xl font-[1000] decoration-8 dark:text-white flex justify-center px-4">
       Ch<h1 className="font-bold text-blue-500 ">^</h1>t Tr
-      <h1 className="font-bold text-blue-500">^</h1>ding 212
+      <h1 className="font-bold text-blue-500">^</h1>ding212
     </h3>
   );
 }
