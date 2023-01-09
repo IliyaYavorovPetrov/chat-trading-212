00caec977ff000bc204203344645ad8d25e4db1c
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-09 18:35:08 +0200

    format

--- chat-client/src/views/popups/ErrorPopup.jsx
+++ chat-client/src/views/popups/ErrorPopup.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 function ErrorPopup({ errorMsg }) {
-    return <div class="fixed inset-0 z-10">
-    <div class="relative max-w-md mx-auto flex items-center justify-center px-6 py-4 bg-red-600 rounded-lg shadow-xl">
-      <div class="text-center font-semibold text-white">
-        {errorMsg}
+  return (
+    <div class="fixed inset-0 z-10">
+      <div class="relative max-w-md mx-auto flex items-center justify-center px-6 py-4 bg-red-600 rounded-lg shadow-xl">
+        <div class="text-center font-semibold text-white">{errorMsg}</div>
       </div>
     </div>
-  </div>
+  );
 }
 
 export default ErrorPopup;
