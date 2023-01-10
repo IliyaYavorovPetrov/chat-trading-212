5f5b7e258831a34885dc8d0b0397b1a48cfa4394
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-10 19:00:56 +0200

    format

--- chat-client/src/views/auth/widgets/Button.jsx
+++ chat-client/src/views/auth/widgets/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-function Button({ text , onClick}) {
+function Button({ text, onClick }) {
   return (
-    <button type="button" className="w-full my-5 py-2 bg-blue-500 shadow-lg shadow-blue-500/50 hover:shadow-blue-500/40 text-white font-semibold rounded-lg" onClick={() => onClick()}>
+    <button
+      type="button"
+      className="w-full my-5 py-2 bg-blue-500 shadow-lg shadow-blue-500/50 hover:shadow-blue-500/40 text-white font-semibold rounded-lg"
+      onClick={() => onClick()}
+    >
       {text}
     </button>
   );
