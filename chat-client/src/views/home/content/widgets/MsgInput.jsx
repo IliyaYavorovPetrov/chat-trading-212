a973ac3705373f7390287f9b55284ab2880125b9
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-10 19:01:07 +0200

    chnage msginput

--- chat-client/src/views/home/content/widgets/MsgInput.jsx
+++ chat-client/src/views/home/content/widgets/MsgInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PlusIconImage from "./PlusIconImage";
 
 const MsgInput = () => (
-  <div className="flex flex-row items-center justify-between fixed left-88 right-8 bottom-2 rounded-lg shadow-lg bg-gray-400 dark:bg-gray-600 px-2 h-12">
+  <div className="flex flex-row items-center justify-between fixed left-88 right-8 bottom-0 rounded-lg shadow-lg bg-gray-400 dark:bg-gray-600 px-2 h-12">
     <PlusIconImage />
     <input
       type="text"
