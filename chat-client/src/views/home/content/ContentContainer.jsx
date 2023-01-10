3b4fb65ad9f9594a46635a763bdcd64908461b0d
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-10 18:57:28 +0200

    add custom scroller

--- chat-client/src/views/home/content/ContentContainer.jsx
+++ chat-client/src/views/home/content/ContentContainer.jsx
@@ -8,7 +8,30 @@ const ContentContainer = () => {
     <div className="flex flex-col bg-gray-300 dark:bg-gray-700 m-0 h-screen w-full overflow-hidden">
       <TopNavigation />
       <div className="flex flex-col items-center h-full w-full mt-0 ml-0 mx-auto px-0 pb-12 overflow-y-scroll">
-        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`}/>
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />{" "}
+        <Msg name="Leon" timestamp="one week ago" text={`Zdr tigur`} />
       </div>
       <MsgInput />
     </div>
