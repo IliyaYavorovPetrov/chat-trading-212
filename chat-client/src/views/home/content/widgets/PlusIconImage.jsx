eb3d39c533504433f4f449eaa6ef059db5b9b005
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-10 19:02:55 +0200

    update plus icon image button

--- chat-client/src/views/home/content/widgets/PlusIconImage.jsx
+++ chat-client/src/views/home/content/widgets/PlusIconImage.jsx
@@ -4,7 +4,7 @@ import { BsPlusCircleFill } from "react-icons/bs";
 const PlusIconImage = () => (
   <BsPlusCircleFill
     size="22"
-    className="text-blue-500 dark:shadow-lg mx-2 dark:text-primary"
+    className="text-blue-500 dark:shadow-lg mx-2 dark:text-primary hover:scale-110 cursor-pointer"
   />
 );
 
