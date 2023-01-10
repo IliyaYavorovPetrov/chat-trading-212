d52d0b9e0b8b246e5f4fb3a3ad6ee1246bf8c73f
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-10 15:51:22 +0200

    add animation close button

--- chat-client/src/views/home/userinfo/widgets/CloseButton.jsx
+++ chat-client/src/views/home/userinfo/widgets/CloseButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CloseButton({ icon, onClick }) {
   return (
     <button className="fixed"type="button" onClick={() => onClick()}>
-      <div className="flex justify-center items-center rotate-45 w-10 h-10 mt-2 ml-2 dark:bg-gray-700 bg-gray-600 transform text-white hover:scale-110 hover:dark:bg-red-500 hover:bg-red-500 rounded-3xl transition-all duration-300">
+      <div className="flex justify-center items-center transform w-10 h-10 mt-2 ml-2 dark:bg-gray-700 bg-gray-600 text-white hover:scale-110 hover:dark:bg-red-500 hover:bg-red-500 hover:rotate-45 rounded-3xl transition-all duration-300">
         {icon}
       </div>
     </button>
