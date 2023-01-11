cec9d732debfe6b1ad2585247e3f58755de03842
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-11 15:32:51 +0200

    update websockets

--- chat-client/src/App.jsx
+++ chat-client/src/App.jsx
@@ -4,10 +4,20 @@ import Register from "./views/auth/Register";
 import Login from "./views/auth/Login";
 import Home from "./views/home/Home";
 import PrivateRoute from "./privateroute/PrivateRoute";
+import SockJsClient from 'react-stomp';
 
 function App() {
   return (
     <div>
+      <SockJsClient 
+        url="http://localhost:8080/ws-message"
+        topics={['/topic/message']}
+        onConnect={() => console.log("Connected!!")}
+        onDisconnect={() => console.log("Disconnected!")}
+        onMessage={(data) => console.log(data)}
+        debug={false}
+      />
+
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
