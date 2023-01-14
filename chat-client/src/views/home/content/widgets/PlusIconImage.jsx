7238783b1173868fe40d6a6619cc700ddc540ec7
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-01-14 15:08:59 +0200

    add cloudinary

--- chat-client/src/views/home/content/widgets/PlusIconImage.jsx
+++ chat-client/src/views/home/content/widgets/PlusIconImage.jsx
@@ -1,38 +1,63 @@
 import React, { useState } from "react";
 import { BsPlusCircleFill } from "react-icons/bs";
-import { CloudinaryContext } from "cloudinary-react";
 
 const PlusIconImage = () => {
-  const [image, setImage] = useState("");
-  const [url, setUrl] = useState("");
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
 
-  const uploadImage = () => {
+  const uploadImage = (event) => {
+    console.log(event.target.files[0].name);
     const data = new FormData();
-    data.append("file", image);
-    data.append("upload_preset", "qewwcqzi");
-    data.append("cloud_name", "dixr3fcy8");
-    fetch(" https://api.cloudinary.com/v1_1/dixr3fcy8/image/upload", {
+    data.append("file", event.target.files[0]);
+    data.append("upload_preset", "zps5pozv");
+    data.append("cloud_name", "duiqnhfar");
+
+    fetch(" https://api.cloudinary.com/v1_1/duiqnhfar/image/upload", {
       method: "post",
       body: data,
     })
       .then((resp) => resp.json())
       .then((data) => {
         console.log("Image url: " + data.url);
-        setUrl(data.url);
-        console.log("Image url: " + data.url);
       })
       .catch((err) => console.log(err));
   };
 
+  // const sendURLToServer = (eventId, userId, url) => {
+  //   fetch(" http://localhost:8080/events/1/images", {
+  //     method: "put",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       eventId: eventId,
+  //       userId: userId,
+  //       url: url,
+  //     }),
+  //   })
+  //     .then(console.log("request send"))
+  //     .catch((err) => console.log(err));
+  // };
+
   return (
-    // <input type="file" onChange={(e) => setImage(e.target.files[0])}>
-      <button type="button" onClick={() => uploadImage()}>
-        <BsPlusCircleFill
-          size="22"
-          className="text-blue-500 dark:shadow-lg mx-2 dark:text-primary hover:scale-110 cursor-pointer"
-        />
-      </button>
-    // </input>
+    <label
+      htmlFor="file-upload"
+      type="file"
+      style={{
+        backgroundImage: { BsPlusCircleFill },
+      }}
+    >
+      <BsPlusCircleFill
+        size="22"
+        className="text-blue-500 dark:shadow-lg mx-2 dark:text-primary hover:scale-110 cursor-pointer"
+      />
+      <input
+        type="file"
+        id="file-upload"
+        onChange={uploadImage}
+        style={{ display: "none" }}
+      />
+    </label>
   );
 };
 
